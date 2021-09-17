fef87aaa11c363ad04a0d10a94418daed4f94847
Wallace Tácio <wallacett.dev@gmail.com>
2021-09-16 21:29:11 -0300

    Adiciona resolução do exercício FGTS ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string salario;
+
+            int taxa = 8;
+
+            Console.WriteLine(" ");
+            Console.WriteLine(@"Olá trabalhador!
+Vamos calcular seu fundo de garantia (FGTS)?");
+            Console.WriteLine(" ");
+
+            Console.Write("Digite o valor do seu salário (R$): ");
+            salario = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            double r = Convert.ToDouble(salario);
+
+            double fgts = (r * taxa) / 100;
+
+            Console.WriteLine($"FGTS: R$ {fgts:N2}");
+        }
+    }
+}
